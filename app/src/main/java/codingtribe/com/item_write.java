740aa08314eab6aaa1703b40728a13dec8e2a190
sincle582 <sincle582@daum.net>
2018-01-05 16:24:25 +0900

    다솔

--- app/src/main/java/codingtribe/com/item_write.java
+++ app/src/main/java/codingtribe/com/item_write.java
@@ -16,11 +16,11 @@ import com.alamkanak.weekview.WeekViewEvent;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 
-
-public class item_write extends Fragment implements MonthLoader.MonthChangeListener, WeekView.EmptyViewClickListener{
+public class item_write extends Fragment implements MonthLoader.MonthChangeListener, WeekView.EmptyViewClickListener {
 
     private WeekView mWeekView;
     private ArrayList<WeekViewEvent> mNewEvents;
@@ -28,16 +28,16 @@ public class item_write extends Fragment implements MonthLoader.MonthChangeListe
     private int mPosition;
 
     static item_write newInstance(int position) {
-        item_write f = new item_write();	//객체 생성
-        Bundle args = new Bundle();					//해당 fragment에서 사용될 정보 담을 번들 객체
-        args.putInt("position", position);				//포지션 값을 저장
-        f.setArguments(args);							//fragment에 정보 전달.
-        return f;											//fragment 반환
+        item_write f = new item_write();    //객체 생성
+        Bundle args = new Bundle();                    //해당 fragment에서 사용될 정보 담을 번들 객체
+        args.putInt("position", position);                //포지션 값을 저장
+        f.setArguments(args);                            //fragment에 정보 전달.
+        return f;                                            //fragment 반환
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mPosition = getArguments() != null ? getArguments().getInt("position") : 0;	// 뷰페이저의 position값을  넘겨 받음
+        mPosition = getArguments() != null ? getArguments().getInt("position") : 0;    // 뷰페이저의 position값을  넘겨 받음
     }
 
     @Nullable
@@ -57,7 +57,29 @@ public class item_write extends Fragment implements MonthLoader.MonthChangeListe
 
         // Initially, there will be no events on the week view because the user has not tapped on
         // it yet.
+        Calendar cal1  = new GregorianCalendar();
+
+        Calendar startOfMonth = Calendar.getInstance();
+        startOfMonth.set(Calendar.YEAR, 2018);
+        startOfMonth.set(Calendar.MONTH, 2 - 1);
+        startOfMonth.set(Calendar.DAY_OF_MONTH, 1);
+        startOfMonth.set(Calendar.HOUR_OF_DAY, 3);
+        startOfMonth.set(Calendar.MINUTE, 40);
+        startOfMonth.set(Calendar.SECOND, 0);
+        startOfMonth.set(Calendar.MILLISECOND, 0);
+        Calendar endOfMonth = (Calendar) startOfMonth.clone();
+        endOfMonth.set(Calendar.DAY_OF_MONTH, endOfMonth.getMaximum(Calendar.DAY_OF_MONTH));
+        endOfMonth.set(Calendar.HOUR_OF_DAY, 23);
+        endOfMonth.set(Calendar.MINUTE, 59);
+        endOfMonth.set(Calendar.SECOND, 59);
+
+        Calendar endTime = (Calendar) cal1.clone();
+        endTime.add(Calendar.MINUTE, 60);
+
+
+
         mNewEvents = new ArrayList<WeekViewEvent>();
+        mNewEvents.add(new WeekViewEvent(20, "olleh",,));
         return v;
     }
 
@@ -72,7 +94,8 @@ public class item_write extends Fragment implements MonthLoader.MonthChangeListe
 
     /**
      * Get events that were added by tapping on empty view.
-     * @param year The year currently visible on the week view.
+     *
+     * @param year  The year currently visible on the week view.
      * @param month The month currently visible on the week view.
      * @return The events of the given year and month.
      */
@@ -105,24 +128,22 @@ public class item_write extends Fragment implements MonthLoader.MonthChangeListe
         }
         return events;
     }
+
     //시간 받는 곳
     @Override
     public void onEmptyViewClicked(Calendar time) {
-
-
         // Set the new event with duration one hour.
         Calendar endTime = (Calendar) time.clone();
         endTime.add(Calendar.MINUTE, 60);
         updateDetail(time, endTime);
 
         // Create a new event.
-//        WeekViewEvent event = new WeekViewEvent(20, "New event", time, endTime);
+        //WeekViewEvent event = new WeekViewEvent(20, "New event", time, endTime);
 
         // mNewEvents.add(event);
 
         // Refresh the week view. onMonthChange will be called again.
-       // mWeekView.notifyDatasetChanged();
-
+        // mWeekView.notifyDatasetChanged();
 
 
     }
@@ -130,7 +151,7 @@ public class item_write extends Fragment implements MonthLoader.MonthChangeListe
     private void updateDetail(Calendar time, Calendar endTime) {
         Intent intent = new Intent(getActivity(), item_write_input.class);
         intent.putExtra("time", time);
-        intent.putExtra("endTime",endTime);
+        intent.putExtra("endTime", endTime);
         startActivity(intent);
     }
 
--- app/src/main/java/codingtribe/com/item_write_input.java
+++ app/src/main/java/codingtribe/com/item_write_input.java
@@ -1,13 +1,39 @@
 package codingtribe.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+
 public class item_write_input extends AppCompatActivity {
+
+    private WeekView mWeekView;
+    private ArrayList<WeekViewEvent> mNewEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_write_input);
+
+        // Get a reference for the week view in the layout.
+        mWeekView = (WeekView)findViewById(R.id.weekView);
+
+        // Initially, there will be no events on the week view because the user has not tapped on
+        // it yet.
+        mNewEvents = new ArrayList<WeekViewEvent>();
+
+        Intent intent = getIntent();
+
+        // Create a new event.
+        //WeekViewEvent event = new WeekViewEvent(20, "New event", time, endTime);
+
+        //mNewEvents.add(event);
+
+        // Refresh the week view. onMonthChange will be called again.
+        mWeekView.notifyDatasetChanged();
     }
 }
