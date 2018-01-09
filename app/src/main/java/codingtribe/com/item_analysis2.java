4bae2757ec127e00a27876b5bce647d7cc022c36
kangjunhee <kang5064@naver.com>
2018-01-09 12:37:24 +0900

    위젯 재생성

--- app/src/main/java/codingtribe/com/item_analysis2.java
+++ app/src/main/java/codingtribe/com/item_analysis2.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Array;
@@ -24,32 +26,32 @@ public class item_analysis2 extends Fragment {
     private int mPosition;
 
     static item_analysis2 newInstance(int position) {
-        item_analysis2 f = new item_analysis2();	//객체 생성
-        Bundle args = new Bundle();					//해당 fragment에서 사용될 정보 담을 번들 객체
-        args.putInt("position", position);				//포지션 값을 저장
-        f.setArguments(args);							//fragment에 정보 전달.
-        return f;											//fragment 반환
+        item_analysis2 f = new item_analysis2();    //객체 생성
+        Bundle args = new Bundle();                    //해당 fragment에서 사용될 정보 담을 번들 객체
+        args.putInt("position", position);                //포지션 값을 저장
+        f.setArguments(args);                            //fragment에 정보 전달.
+        return f;                                            //fragment 반환
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mPosition = getArguments() != null ? getArguments().getInt("position") : 0;	// 뷰페이저의 position값을  넘겨 받음
+        mPosition = getArguments() != null ? getArguments().getInt("position") : 0;    // 뷰페이저의 position값을  넘겨 받음
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_item_analysis2, container, false);	//미리 알고 있는 레이아웃을 inflate 한다.
+        View v = inflater.inflate(R.layout.activity_item_analysis2, container, false);    //미리 알고 있는 레이아웃을 inflate 한다.
 
-        btn_statics = (Button)v.findViewById(R.id.button1);
-        btn_predict = (Button)v.findViewById(R.id.button2);
-        btn_type = (Button)v.findViewById(R.id.button3);
+        btn_statics = (Button) v.findViewById(R.id.button1);
+        btn_predict = (Button) v.findViewById(R.id.button2);
+        btn_type = (Button) v.findViewById(R.id.button3);
 
 
         btn_statics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(),"응답",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "응답", Toast.LENGTH_SHORT).show();
                 updateDetail();
             }
         });
@@ -58,31 +60,28 @@ public class item_analysis2 extends Fragment {
             @Override
             public void onClick(View view) {
 
+                //예측 새창 띄우기
+                updateDetail_predict();
+
                 //찬울 테스트 이제 완성입니다. makerStatTable 메소드는 매시간별로 어떤 행동을 했는지 판단하여 기록된 모든 정보를 시간단위로 각 카테고리명을  ArrayList 형태로 반환해줍니다.
                 ActionDB actionDB = new ActionDB(getContext());
                 ArrayList<ActionVO> actionArrayList = actionDB.getAllAction(getActivity());
                 ActPreProcessor act = new ActPreProcessor();
-                act.makeStatTable(actionArrayList);
 
-                //예측 새창 띄우기
-                updateDetail_predict();
                 //SQLite에서 DB값 가져와 서버로 보내기
-                JsonCreate jc = new JsonCreate();
-                String sendmsg = "vision_write";
-                String result = jc.createJson(getActivity()).toString(); //JSONArray 형태로 리턴.
+                //JsonCreate jc = new JsonCreate();
+                //String result = jc.createJson(getActivity()).toString(); //JSONArray 형태로 리턴.
                 //Log.v("jason",result);
-                try{
 
-                    new JsonSend(sendmsg).execute(result,"vision_write").get();//보내는것
-
-                }catch (Exception e){
-
+                //찬울 테스트 데이터 JSON으로 보내기
+                String sendmsg = "vision_write";
+                String result = null;
+                try {
+                    result = act.makeStatTable(actionArrayList);
+                    new JsonSend(sendmsg).execute(result, "vision_write").get();//보내는것
+                } catch (Exception e) {
                     e.printStackTrace();
-
                 }
-
-
-
             }
         });
 
@@ -101,14 +100,14 @@ public class item_analysis2 extends Fragment {
     private void updateDetail() {
 
         Intent intent = new Intent(getActivity(), pie_pie.class);
-        intent.putExtra("year",Calendar.getInstance().get(Calendar.YEAR));
-        intent.putExtra("month",Calendar.getInstance().get(Calendar.MONTH));
-        intent.putExtra("date",Calendar.getInstance().get(Calendar.DATE));
+        intent.putExtra("year", Calendar.getInstance().get(Calendar.YEAR));
+        intent.putExtra("month", Calendar.getInstance().get(Calendar.MONTH));
+        intent.putExtra("date", Calendar.getInstance().get(Calendar.DATE));
         startActivity(intent);
     }
 
 
-    private void updateDetail_predict(){
+    private void updateDetail_predict() {
         Intent intent = new Intent(getActivity(), item_predict.class);
         startActivity(intent);
     }
--- app/src/main/java/codingtribe/com/item_predict.java
+++ app/src/main/java/codingtribe/com/item_predict.java
@@ -2,6 +2,11 @@ package codingtribe.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class item_predict extends AppCompatActivity {
 
@@ -9,8 +14,54 @@ public class item_predict extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_predict);
+
 //predict 작성 시작
+//서버에서 String 데이터를 전송 받을 때
+//    private void receiveObject(JSONObject data) {
+//
+//        try {
+//            mReceiveTv.setText(data.toString());
+//            mReceiveNationTv.setText("nation : " + data.getString("nation"));
+//            mReceiveNameTv.setText("name : " + data.getString("nation"));
+//            mReceiveAddressTv.setText("address : " + data.getString("address"));
+//            mReceiveAgeTv.setText("age : " + data.getString("age"));
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
 
-
+//
+//        // 서버에서 배열 데이터를 전송 받을 때
+//
+//    private void receiveArray(String dataObject) {
+//        recyclerView.setVisibility(View.VISIBLE);
+//        objectResultLo.setVisibility(View.GONE);
+//        mItems.clear();
+//        try {
+//// String 으로 들어온 값 JSONObject 로 1차 파싱
+//            JSONObject wrapObject = new JSONObject(dataObject);
+//
+//// JSONObject 의 키 "list" 의 값들을 JSONArray 형태로 변환
+//            JSONArray jsonArray = new JSONArray(wrapObject.getString("list"));
+//            for (int i = 0; i < jsonArray.length(); i++) {
+//// Array 에서 하나의 JSONObject 를 추출
+//                JSONObject dataJsonObject = jsonArray.getJSONObject(i);
+//// 추출한 Object 에서 필요한 데이터를 표시할 방법을 정해서 화면에 표시
+//// 필자는 RecyclerView 로 데이터를 표시 함
+//                mItems.add(new Item(dataJsonObject.getString("nation") + i, dataJsonObject.getString("name") + i,
+//                        dataJsonObject.getString("address") + i, dataJsonObject.getString("age")));
+//            }
+//// Recycler Adapter 에서 데이터 변경 사항을 체크하라는 함수 호출
+//            adapter.notifyDataSetChanged();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
     }
 }
+
+
+
+
+
+
+
+
